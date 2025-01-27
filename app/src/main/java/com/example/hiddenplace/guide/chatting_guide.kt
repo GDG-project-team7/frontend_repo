73fbde664e1fdf627dc0595f7d0f6d4d9d11a50a
ahnsohyeong <sunnyy1022@gmail.com>
2025-01-28 02:41:20 +0900

    guide_main view, estimate_list view, chatting_guide view, MainActivity code change

--- app/src/main/java/com/example/hiddenplace/guide/chatting_guide.kt
+++ app/src/main/java/com/example/hiddenplace/guide/chatting_guide.kt
@@ -1,17 +1,17 @@
-package com.example.hiddenplace
+package com.example.hiddenplace.guide
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.hiddenplace.R
 
-class MainActivity : AppCompatActivity() {
+class chatting_guide : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_chatting_guide)
 
     }
 }
