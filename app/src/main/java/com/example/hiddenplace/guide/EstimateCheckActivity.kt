09551a8724a988f0c67c3b1068477b2e557b1cec
subin <asnm07@naver.com>
2025-01-29 06:00:37 +0900

    test

--- app/src/main/java/com/example/hiddenplace/guide/EstimateCheckActivity.kt
+++ app/src/main/java/com/example/hiddenplace/guide/EstimateCheckActivity.kt
@@ -1,6 +1,8 @@
 package com.example.hiddenplace.guide
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,10 +14,22 @@ class EstimateCheckActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_estimate_check)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // 수락 버튼
+        val acceptanceButton = findViewById<Button>(R.id.acceptance_button)
+        acceptanceButton.setOnClickListener {
+            // SendPortfolioActivity로 이동
+            val intent = Intent(this, SendPortfolioActivity::class.java)
+            startActivity(intent)
         }
+
+        // 거절 버튼
+        val rejectionButton = findViewById<Button>(R.id.rejection_button)
+        rejectionButton.setOnClickListener {
+            // EstimateRejectionActivity로 이동
+            val intent = Intent(this, EstimateRejectionActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/hiddenplace/guide/EstimateRejectionActivity.kt
+++ app/src/main/java/com/example/hiddenplace/guide/EstimateRejectionActivity.kt
@@ -12,10 +12,8 @@ class EstimateRejectionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_estimate_rejection)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+
+
     }
 }
