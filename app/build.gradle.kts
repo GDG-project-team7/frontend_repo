ea379833ecad8608ef1712eead6bc1c908273f7a
subin <asnm07@naver.com>
2025-02-25 12:17:33 +0900

    ocr,portfolio

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,9 @@ plugins {
 android {
     namespace = "com.example.hiddenplace"
     compileSdk = 35
+    buildFeatures {
+        viewBinding = true
+    }
 
     defaultConfig {
         applicationId = "com.example.hiddenplace"
