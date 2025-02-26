a169f90b3842e25f006f8ed6bc6ce7bec2cf6236
subin <asnm07@naver.com>
2025-02-26 15:08:29 +0900

    머지 후 코드 수정

--- app/src/main/java/com/example/hiddenplace/auth/JoinResponseModel.kt
+++ app/src/main/java/com/example/hiddenplace/auth/JoinResponseModel.kt
@@ -1,8 +1,5 @@
 package com.example.hiddenplace.auth
 
 data class JoinResponseModel(
-    val userId: String,
-    val userName: String,
-    val email: String,
-    val isGuide: Boolean
+    val userId: Int
 )
